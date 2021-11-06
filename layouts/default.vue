28ac7821f9a5ad8eacf994112f3279619e35b915
ryo-n-cell <nakamuraryota582@gmail.com>
2021-11-06 22:53:50 +0900

    211106

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,10 @@
 <template>
-  <v-app dark>
-    <v-app-bar fixed app>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-    </v-app-bar>
+  <v-app>
     <v-main>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <nuxt />
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer>
+      <span>&copy; Ryota Nakamura {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
@@ -29,7 +13,7 @@
 export default {
   data() {
     return {
-      title: 'ITエンジニアあるあるアプリ',
+      title: 'SORENA',
     }
   },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,54 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card>
-        <v-card-title class="headline">
-          ようこそ！「エンジニアあるあるアプリ(v0.1)」へ！
-        </v-card-title>
-        <v-card-text>
-          <p>※こちらはNuxt.js+Vuetifyを使用しています</p>
-          <hr class="my-3" />
-          <h1>アプリ内容</h1>
-          <p>
-            ITエンジニアによくある事象はネットでよく見かけるけど、果たして皆そうなのか？
-          </p>
-          <p>
-            みんなが手軽に答えれるアプリがあればいいよね。という思いから作成したアプリケーションです。
-          </p>
-          <p>本アプリUIはtinderに大きく影響を与えています。</p>
-          <p>
-            また、別会社様のエンジニアさんと協働で開発しています（僕=>Web版とバックエンド、インフラ回り/エンジニアさん=>ネイティブアプリ版のみ）
-          </p>
-          <p></p>
-          <div class="text-xs-right"></div>
-          <hr class="my-3" />
-          <h1>やりかた</h1>
-          <p>１，以下の「CONTINUE」ボタンをタップ</p>
-          <p>
-            ２、カードが出てくるのでスワイプして答える。（左：ないない/右：あるよねー）
-          </p>
-          <p>３、結果が出てきます</p>
-          <p>
-            ※WebAPIはHerokuの無料アカウントへデプロイしてるのでAPI呼び出しに時間がかかります。
-            よって、メインページの読み込みに時間がかかります。
-            実用化したら有料アカウントへ移行して常に表示できる形にします
-            （常にAPIを起こすバッチも作れますが、Heroku Loveなので課金しますw）
-          </p>
-          <hr class="my-3" />
-          <h1>おおまかなロードマップ</h1>
-          <p>・プロトタイプのフロントとバックエンドAPIを作る←ココまでは完成</p>
-          <p>・トランザクション処理などのDBの設計構築</p>
-          <p>=>最小から最大スケールまで設計済なので、まずは最小から構築</p>
-          <p>・DBと繋げて、各種APIを作成</p>
-          <p>
-            =>DBにある問題をランダマイズしたAPIや今までの利用者が答えた結果を表示するAPIを作る。
-          </p>
-          <p>・フロント側の改良やら色々</p>
-          <p>=>色々グチャグチャなので整理。また、メインページのUX周りも整備</p>
-          <p>フォーム回りを作成</p>
-          <p>
-            ペネトレーションテスト(SQLインジェクション、Dos攻撃対策はされてるか念のため)
-          </p>
-          <p>負荷テスト（Webとスマホ両方</p>
-          <p>＝＝＝＝ここまでがv1＝＝＝＝</p>
-          <p>Twitterとの連携（OAuth認証を使うかも）</p>
-          <p>
-            ・「デザイナーあるある」等の別系統を作成するためにDBを最大スケールまで構築
-          </p>
-          <p>・DBと繋げて、各種APIを作成</p>
-          <p>etc....</p>
-          <hr class="my-3" />
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
+  <v-container fluid pa-0>
+    <v-row no-gutters class="topView" justify="center" align="center">
+      <v-col pt-0>
+        <v-img class="topImg" :src="image_src">
+          <v-row id="topInnner" no-gutters justify="center" align="center">
+            <v-col cols="10">
+              <p id="topLetter">
+                みんなの 「<strong id="strLetter">それな</strong
+                >」をシェアしよう！
+              </p>
+            </v-col>
+          </v-row>
+        </v-img>
+        <!-- <v-spacer />
           <v-btn color="primary" nuxt to="/mainPage"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+        </v-card-actions> -->
+      </v-col>
+    </v-row>
+    <v-row no-gutters class="topView-bottom" justify="center" align="center">
+      <v-btn x-large color="success" dark>やってみる！</v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      image_src: require('@/assets/img/top_img/sorena_top.jpg'),
+    }
+  },
+}
 </script>
+<style>
+.topView {
+  height: 80vh;
+}
+.topImg {
+  height: 60vh;
+}
+#topInnner {
+  height: 100%;
+}
+#strLetter {
+  font-size: 16px;
+}
+#topLetter {
+  font-size: 14px;
+  color: white;
+  background-color: black;
+  text-align: center;
+}
+</style>
