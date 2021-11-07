b15fa82eb19730df2c3242798a50f031e1842c06
ryo-n-cell <nakamuraryota582@gmail.com>
2021-11-07 14:42:08 +0900

    211107-1441仮組み完成SP

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    { src: '~assets/css/styles.scss' }
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
